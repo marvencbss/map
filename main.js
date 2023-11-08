3bae50164b2576cb1f8d73d3e050ace3b11e15d4
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-11-08 20:29:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const srv = express();
+const configs = require('./package.json');
 
 srv.listen(3030, servidorAguardando);
 
+srv.get('/dizoi/:nome', (req, res)=>{
+    res.send(`oi ${req.params.nome}`)
+});
+
+srv.get('/dizoi/:nome/x/vezes', (req, res)=>{
+    let ois ='';
+    for(i=0; i<req.params.vezes; i++){
+        ois = ois + res.send(`oi ${req.params.nome}`)
+    }
+});
+
 srv.get('/info', (req, res)=>{
     info = {
-        nome: 'servidor do mestre',
-        versao: 'v1.0.01.21',
-        licenca: 'MIT',
+        nome: configs.name,
+        versao: configs.version,
+        autor: configs.author
+    }   
+        res.json(info);
     }
-    res.json(info);
-});
+);
 srv.get('/json', getRootJSON);
 srv.get('/html', getRootHTML);
 
@@ -38,8 +51,8 @@ function getRootHTML(pedido, resposta) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://unpkg.com/mvp.css"> 
-        <title>Pagina</title>
+        <link rel="stylesheet" href="https://unpkg.com/simpledotcss/simple.min.css"> 
+        <title>se vc ta lendo isso vc e mestre</title>
     </head>
     <body>
     <header>o titulo dos mestres</header>
