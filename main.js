2b5a8816afe4c9250ae6afd90e43786b8879c077
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-11-13 21:28:59 -0300

    respostas json/html/insert

--- main.js
+++ main.js
@@ -2,7 +2,52 @@ const express = require('express');
 const sqlite3 = require('sqlite3');
 const srv = express();
 const configs = require('./package.json');
+srv.use(require('body-parser').urlencoded({ extended: false }));
 srv.listen(3030, servidorAguardando);
+
+srv.get('/', (req, res)=>{
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>bem vindo ao sitezao</title>
+    </head>
+    <body>
+        este é o melhor site de toda a história da humanidade<br><br><br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium sint porro officia explicabo aliquam quia perspiciatis maiores necessitatibus ea exercitationem quaerat nihil reprehenderit iste facere optio delectus asperiores, illum provident!<br>
+        Mussum Ipsum, cacilds vidis litro abertis.  Nulla id gravida magna, ut semper sapien. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Manduma pindureta quium dia nois paga. Negão é teu passadis, eu sou faxa pretis.
+    
+    <br>Leite de capivaris, leite de mula manquis sem cabeça. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Bota 1 metro de cachacis aí pra viagem! Delegadis gente finis, bibendum egestas augue arcu ut est.
+    
+    <br>Nulla id gravida magna, ut semper sapien. Eu nunca mais boto a boca num copo de cachaça, agora eu só uso canudis! A ordem dos tratores não altera o pão duris. Delegadis gente finis, bibendum egestas augue arcu ut est.
+    
+    <br>Quem num gosta di mim que vai caçá sua turmis! Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Atirei o pau no gatis, per gatis num morreus. Interagi no mé, cursus quis, vehicula ac nisi.
+    
+    <br>Cevadis im ampola pa arma uma pindureta. Leite de capivaris, leite de mula manquis sem cabeça. Diuretics paradis num copo é motivis de denguis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.
+    
+    <br>Interagi no mé, cursus quis, vehicula ac nisi. Nulla id gravida magna, ut semper sapien. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Quem num gosta di mé, boa gentis num é.
+    
+    <br>Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Diuretics paradis num copo é motivis de denguis. Leite de capivaris, leite de mula manquis sem cabeça.
+    
+    <br>Segunda-feiris nun dá, eu vô me pirulitá! Copo furadis é disculpa de bebadis, arcu quam euismod magna. Diuretics paradis num copo é motivis de denguis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+    
+    <br>Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Suco de cevadiss deixa as pessoas mais interessantis. A ordem dos tratores não altera o pão duris.
+    
+    <br>Casamentiss faiz malandris se pirulitá. Ainda é segunda-feris e já tô sem paciêncis! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. A ordem dos tratores não altera o pão duris.
+    
+    <br>Quem num gosta di mim que vai caçá sua turmis! Sapien in monti palavris qui num significa nadis i pareci latim. Segunda-feiris nun dá, eu vô me pirulitá! Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.
+        
+    </body>
+    </html>`)
+});
+
+srv.post('/registrar', (req, res)=>{
+    console.log(req.body.nome);
+    console.log(req.body.email);
+    console.log(req.body.msg);
+    res.send(`Um recado recebido: Para ${req.body.nome}, ${req.body.email}
+     Recado: ${req.body.msg}`);
+    });
 
 srv.get('/deixaroi/:nome/:msg' , (req, res)=>{
     const db = new sqlite3.Database('mensagens.db');
@@ -21,7 +66,6 @@ srv.get('/deixaroi/:nome/:msg' , (req, res)=>{
         }
     });
 })
-
 
 srv.get('/dizoi/:nome', (req, res)=>{
     const db = new sqlite3.Database('mensagens.db');
